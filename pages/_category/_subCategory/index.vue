312300e361d8f7bdf9b22916648c29cff9c7593f
Martin Novák <martin.novak@cleverlance.com>
2020-04-14 09:43:52 +0200

    feat: vypsani stranek v hiearchii kategorie a subkategorie

---
+++ pages/_category/_subCategory/index.vue
@@ -0,0 +1,20 @@
+<template>
+<!--  <div>Hello {{ dog.id }}</div>-->
+  <div>{{ test }}</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'dog',
+  asyncData(context) {
+    return { test: context.params.subCategory }
+    //return axios.get(`https://api.thedogapi.com/v1/images/${context.params.slug}`)
+    //  // eslint-disable-next-line arrow-body-style
+    //  .then(({ data }) => {
+    //    return { dog: data };
+    //  });
+  },
+};
+</script>
